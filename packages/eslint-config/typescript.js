c77b6756fc92444f2a655169e486470841ad7b90
Joosung Park <iam@hyperlapse.dev>
2025-02-26 21:04:17 +0900

    chore(web): enable prettier and eslint

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -1,6 +1,18 @@
 import tseslint from 'typescript-eslint';
 import baseConfig from './base.js';
 
-const config = tseslint.config(...baseConfig, tseslint.configs.strict);
+const config = [
+  ...tseslint.config(...baseConfig, tseslint.configs.strict),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+];
 
 export default config;
